9056595b9e178eaf4cf967e79ad055b855e4c370
Magali <magalilentino@gmail.com>
2025-09-10 10:51:26 -0300

    primer controller

---
+++ src/main/java/com/particulares/tp/java/controller/ProvinciaController.java
@@ -0,0 +1,78 @@
+package com.particulares.tp.java.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.particulares.tp.java.service.ProvinciaService;
+
+@Controller
+@RequestMapping("/provincia") // localhost:8080/provicia
+public class ProvinciaController {
+
+    @Autowired
+    private ProvinciaService provinciaServicio;
+
+    @GetMapping("/registrar") 
+    public String registrar() {
+        return "provincia_form.html";
+    }
+
+    @PostMapping("/crear") // localhost:8080/provincia/crear
+    public String crear(@RequestParam String nombre, ModelMap modelo){
+        try {
+            provinciaServicio.crearProvincia(nombre);   // llamo a mi servicio para persistir  
+            modelo.put("exito", "La provincia fue cargada correctamente");
+        
+        } catch (Exception ex) {
+            modelo.put("error", ex.getMessage());
+            return "provincia_form.html";
+        }        
+        return "inicio.html";
+}
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo) {
+
+        modelo.addAttribute("provincias", provinciaServicio.listarProvincias()); 
+
+        return "provincia_list.html";
+    }
+
+    //getOne
+    @GetMapping("/modificar/{id}")
+    public String modificar(@PathVariable int id, ModelMap modelo) {
+        modelo.put("provincia", provinciaServicio.getOne(id));
+
+        return "provincia_modificar.html";
+    }
+
+
+    @PostMapping("/modificar/{id}")
+    public String modificar(@PathVariable int id, @RequestParam String nombre, ModelMap modelo) {
+        try {
+            provinciaServicio.modificarProvincia(nombre, id);
+
+            return "redirect:../lista";
+            
+        } catch (Exception ex) {
+            modelo.put("error", ex.getMessage());
+            return "provincia_modificar.html";
+        }
+    }
+
+    @PostMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable int id, ModelMap modelo) {
+        try {
+            provinciaServicio.eliminarProvincia(id);
+            return "redirect:../lista";
+        } catch (Exception ex) {
+            modelo.put("error", ex.getMessage());
+            return "provincia_list.html";
+        }
+    }
+}
--- src/main/java/com/particulares/tp/java/service/DictadoClaseService.java
+++ src/main/java/com/particulares/tp/java/service/DictadoClaseService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.particulares.tp.java.entities.Materia;
@@ -16,6 +17,7 @@ import com.particulares.tp.java.repository.MateriaRepository;
 import com.particulares.tp.java.repository.ProfesorRepository;
 import com.particulares.tp.java.repository.DictadoClaseRepository;
 
+@Service
 public class DictadoClaseService {
     @Autowired
     private DictadoClaseRepository dictadoClaseRepository;
