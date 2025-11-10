79aed67336bca02216e80cd59a4ef85860e9099f
Magali <magalilentino@gmail.com>
2025-11-10 12:35:38 -0300

    estilos

--- src/main/java/com/particulares/tp/java/controller/LoginController.java
+++ src/main/java/com/particulares/tp/java/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
 
     @GetMapping("/")  
     public String index() {
-        return "index.html";   
+        return "index";   
     }
 
     @GetMapping("/registrar") 
