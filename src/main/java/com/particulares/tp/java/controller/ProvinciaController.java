b2a21cb99701494556cc30abc59a30170e0f9784
Valeria <valelupopiatti@gmail.com>
2025-11-10 14:50:00 -0300

    interfaz admin y fin cruds provincia y localidad

--- src/main/java/com/particulares/tp/java/controller/ProvinciaController.java
+++ src/main/java/com/particulares/tp/java/controller/ProvinciaController.java
@@ -1,5 +1,4 @@
 package com.particulares.tp.java.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,12 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.particulares.tp.java.entities.Provincia;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.particulares.tp.java.service.ProvinciaService;
 
+
 @Controller
-@RequestMapping("/provincia") // localhost:8080/provicia
+@RequestMapping("/provincia") 
 public class ProvinciaController {
 
     @Autowired
@@ -21,10 +20,10 @@ public class ProvinciaController {
 
     @GetMapping("/registrar") 
     public String registrar() {
-        return "provincia/form";
+        return "admin/provincia/form";
     }
 
-    @PostMapping("/crear") // localhost:8080/provincia/crear
+    @PostMapping("/crear") 
     public String crear(@RequestParam String nombre, ModelMap modelo){
         try {
             provinciaServicio.crearProvincia(nombre);   // llamo a mi servicio para persistir  
@@ -33,49 +32,26 @@ public class ProvinciaController {
         } catch (Exception ex) {
             modelo.put("error", ex.getMessage());
         }        
-        return "provincia/form";
-}
+        return "admin/provincia/form";
+    }
 
-
-    @GetMapping("/lista")
+    @GetMapping("/listar")
     public String listar(ModelMap modelo) {
 
         modelo.addAttribute("provincias", provinciaServicio.listarProvincias()); 
-        return "provincia/lista";
+        return "admin/provincia/lista";
     }
 
-
-    // @GetMapping("/modificar/{id}")
-    // public String modificar(@PathVariable int id, ModelMap modelo) {
-    //     modelo.put("provincia", provinciaServicio.getOne(id));
-
-    //     return "provincia_modificar.html";
-    // }
-
-
-    // @PostMapping("/modificar/{id}")
-    // public String modificar(@PathVariable int id, @RequestParam String nombre, ModelMap modelo) {
-    //     try {
-    //         provinciaServicio.modificarProvincia(nombre, id);
-
-    //         return "redirect:../lista";
-            
-    //     } catch (Exception ex) {
-    //         modelo.put("error", ex.getMessage());
-    //         return "provincia_modificar.html";
-    //     }
-    // }
-
-    // @PostMapping("/eliminar/{id}")
-    // public String eliminar(@PathVariable int id, ModelMap modelo) {
-    //     try {
-    //         provinciaServicio.eliminarProvincia(id);
-    //         return "redirect:../lista";
-    //     } catch (Exception ex) {
-    //         modelo.put("error", ex.getMessage());
-    //         return "provincia_list.html";
-    //     }
-    // } me parece que no hay que eliminar 
+    @PostMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable int id, RedirectAttributes redirectAttributes) {
+        try {
+            provinciaServicio.eliminarProvincia(id);
+            redirectAttributes.addFlashAttribute("exito", "Provincia eliminada correctamente.");
+        } catch (Exception ex) {
+            redirectAttributes.addFlashAttribute("error", ex.getMessage());
+        }
+        return "redirect:/provincia/listar";
+    } 
 
 }
 
